44e0d90c26ab0bc78276f21803d7edf56793190a
a-saadi <anouarsaadi1802@gmail.com>
2021-08-09 18:56:55 +0100

    (webserv): Editing in  Makefile

--- server.hpp
+++ server.hpp
@@ -1,4 +1,30 @@
 #ifndef SERVER_HPP
 # define SERVER_HPP
 
+# include <iostream>
+# include <string>
+# include <sys/socket.h>
+# include <sys/types.h>
+# include <limits.h>
+
+class server
+{
+private:
+    int _serverFd;
+    int _accptFd;
+
+public:
+    server();
+    ~server();
+};
+
+server::server()
+{
+}
+
+server::~server()
+{
+}
+
+
 #endif
